250af43c6f33d130100804b7598efde58f3f82ee
DevNul <devnulpavel@gmail.com>
2018-10-05 09:07:11 +0300

    Another code improvement

--- Arduino/IRRemote/IRRemote.ino
+++ Arduino/IRRemote/IRRemote.ino
@@ -1,7 +1,7 @@
 #include <IRremote.h>
 
 #define IR_DIGITAL_PIN 2
-#define FAN_PIN 3
+#define FAN_PIN 9
 
 IRrecv irrecv(IR_DIGITAL_PIN); // указываем вывод, к которому подключен приемник
 bool fanEnabled = false;
@@ -11,11 +11,19 @@ unsigned int lastVal = 0;
 
 void setup() {
     Serial.begin(115200); // выставляем скорость COM порта
-    //irrecv.enableIRIn(); // запускаем прием
+    irrecv.enableIRIn(); // запускаем прием
+
+    // http://kazus.ru/forums/showthread.php?t=107888
+    // Частота ШИМ для 3 ноги 3906.25гц
+    //TCCR2B = TCCR2B & 0b11111000 | 0x02;
+    // Частота ШИМ для 5 ноги 7812.5гц
+    // TCCR0B = TCCR0B & 0b11111000 | 0x02;
+    // Частота ШИМ для 9 ноги 31250гц
+    TCCR1B = TCCR1B & 0b11111000 | 0x01;
 }
 
 void loop() {
-    /*decode_results results;
+    decode_results results;
     while(irrecv.decode(&results)) { // если данные пришли
         unsigned int curVal = 0;
         if(results.value == 0xFFFFFFFF){
@@ -34,6 +42,7 @@ void loop() {
                 break;
             case 0x9D62C837: // ON OFF
                 fanEnabled = !fanEnabled;
+                fanSpeed = 255;
                 break;
         }
         irrecv.resume(); // принимаем следующую команду
@@ -48,12 +57,11 @@ void loop() {
         Serial.print(" ");
         Serial.print(fanSpeed);
         Serial.println();
-    }*/
+    }
 
-    /*if(fanEnabled){
+    if(fanEnabled){
         analogWrite(FAN_PIN, fanSpeed);
     }else{
         analogWrite(FAN_PIN, 0);
-    }*/
-    analogWrite(FAN_PIN, 255);
+    }
 }
