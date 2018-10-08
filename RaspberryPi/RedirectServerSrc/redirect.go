8a2533028549de2c112be0615ec3baced13512a5
Pavel Ershov <devnulpavel@gmail.com>
2018-10-08 12:30:15 +0300

    Raspberry pi camera support

--- RaspberryPi/RedirectServerSrc/redirect.go
+++ RaspberryPi/RedirectServerSrc/redirect.go
@@ -7,6 +7,7 @@ import (
     "fmt"
     "strconv"
     "strings"
+    "os/exec"
     "encoding/gob"
     "net/http"
     "image/jpeg"
@@ -37,11 +38,13 @@ const MAIN_PAGE = `
     <title>Main page</title>
 </head>
 <body>
-    <p><a href="odroid">Odroid status</a></p>
     <p><a href="esp">ESP8268 status</a></p>
-    <p><a href="image">Digma image</a></p>
-    <p><a href="small">Digma small image</a></p>
-    <p><a href="video">Digma video</a></p>
+    <p><a href="digma_image">Digma image</a></p>
+    <p><a href="digma_small">Digma small image</a></p>
+    <p><a href="digma_video">Digma video</a></p>
+    <p><a href="pi_image?shutter_time_ms=4000&iso=800">Pi image (Night)</a></p>
+    <p><a href="pi_image?shutter_time_ms=1000&iso=160">Pi image (Day)</a></p>
+    <p><a href="pi_custom">Pi image (Custom)</a></p>
 </body>
 </html>
 `
@@ -62,6 +65,30 @@ const LOGIN_PAGE = `
           <input type="text" placeholder="Username" name="login">
           <input type="text" placeholder="Password" name="pass">
           <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+</body>
+</html>
+`
+
+//////////////////////////////////////////
+// Login page
+
+const CUSTOM_PI_CAMERA_PAGE = `
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Main page</title>
+</head>
+<body>
+    <div>
+      <div>
+        <form action="/pi_image" method="get">
+          Shutter time (mSec): <input type="text" placeholder="in mSec" value="330" name="shutter_time_ms">
+          ISO: <input type="text" placeholder="100-800" value="800" name="iso">
+          <br>
+          <button type="submit">Go to image</button>
         </form>
       </div>
     </div>
@@ -141,15 +168,15 @@ func sharedDigmaHandler(w http.ResponseWriter, r *http.Request, command string) 
     }
 }
 
-func image(w http.ResponseWriter, r *http.Request) {
+func digmaImage(w http.ResponseWriter, r *http.Request) {
     sharedDigmaHandler(w, r, "snapshot.cgi")
 }
 
-func video(w http.ResponseWriter, r *http.Request) {
+func digmaVideo(w http.ResponseWriter, r *http.Request) {
     sharedDigmaHandler(w, r, "videostream.cgi")
 }
 
-func imageSmall(w http.ResponseWriter, r *http.Request) {
+func digmaImageSmall(w http.ResponseWriter, r *http.Request) {
     if !checkLogin(w, r) {
         return
     }
@@ -227,6 +254,64 @@ func esp(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func getPiCameraImage(shutterTimeMSec uint32, iso uint32) ([]byte, error) {
+    commandText := fmt.Sprintf("raspistill -vf -hf -ss %d -ISO %d -o -", shutterTimeMSec*1000, iso)
+    command := exec.Command("bash", "-c", commandText)
+    imageResult, err := command.Output()
+    if err != nil {
+        log.Printf("%s", err)
+        return nil, err
+    }
+
+    return imageResult, nil
+}
+
+func piImage(w http.ResponseWriter, r *http.Request) {
+    if !checkLogin(w, r) {
+        return
+    }
+
+    var shutterTime uint32 = 330
+    var iso uint32 = 800
+    
+    shutterTimeGet, ok := r.URL.Query()["shutter_time_ms"]
+    if ok && len(shutterTimeGet[0]) > 0 {
+        parsedVal, err := strconv.ParseInt(shutterTimeGet[0], 10, 32)
+        if err == nil {
+            shutterTime = uint32(parsedVal)
+        }
+    }
+
+    isoGet, ok := r.URL.Query()["iso"]
+    if ok && len(isoGet[0]) > 0 {
+        parsedVal, err := strconv.ParseInt(isoGet[0], 10, 32)
+        if err == nil {
+            iso = uint32(parsedVal)
+        }
+    }
+
+    //log.Printf("Pi image request params: %d %d", shutterTime, iso)
+
+    imageData, imageError := getPiCameraImage(shutterTime, iso)
+    if imageError == nil {
+        w.Header().Set("Content-Type", "image/jpeg")
+        w.Header().Set("Content-Length", strconv.Itoa(len(imageData)))
+        w.Write(imageData)
+    }else{
+        errorString := imageError.Error()
+        w.Header().Set("Content-Type", "text/html")
+        w.Header().Set("Content-Length", strconv.Itoa(len(errorString)))
+        io.WriteString(w, errorString)
+    }
+}
+
+func piImageCustom(w http.ResponseWriter, r *http.Request) {
+    if !checkLogin(w, r) {
+        return
+    }
+    fmt.Fprint(w, CUSTOM_PI_CAMERA_PAGE)
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
     ses, err := cookieStore.Get(r, cookieName)
     if err != nil {
@@ -272,7 +357,6 @@ func root(w http.ResponseWriter, r *http.Request) {
     if !checkLogin(w, r) {
         return
     }
-
     fmt.Fprint(w, MAIN_PAGE)
 }
 
@@ -283,9 +367,11 @@ func main() {
     http.HandleFunc("/login", login)
     http.HandleFunc("/odroid", odroid)
     http.HandleFunc("/esp", esp)
-    http.HandleFunc("/image", image)
-    http.HandleFunc("/video", video)
-    http.HandleFunc("/small", imageSmall)
+    http.HandleFunc("/digma_image", digmaImage)
+    http.HandleFunc("/digma_video", digmaVideo)
+    http.HandleFunc("/digma_small", digmaImageSmall)
+    http.HandleFunc("/pi_image", piImage)
+    http.HandleFunc("/pi_custom", piImageCustom)
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
