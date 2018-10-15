5110589b8c8fdf0a14cdb326678063dfde58295b
DevNul <devnulpavel@gmail.com>
2018-10-15 22:28:08 +0300

    Fade in example

---
+++ Attiny13/FadeInStart/FadeInStart.ino
@@ -0,0 +1,43 @@
+#define OUT_PIN 0
+#define BUTTON_PIN 1
+#define FADE_IN_TIME_SEC 1.5f
+
+bool enabled = false;
+
+void setup() {
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  
+  digitalWrite(OUT_PIN, LOW);
+}
+
+void loop() {
+    bool buttonStatus = (digitalRead(BUTTON_PIN) == HIGH);
+    if(buttonStatus && !enabled){
+        delay(10);
+        buttonStatus = (digitalRead(BUTTON_PIN) == HIGH);
+
+        if(buttonStatus){           
+            enabled = true;
+
+            digitalWrite(OUT_PIN, HIGH);
+            delay(500);
+            
+            const int delayValue = (int)(FADE_IN_TIME_SEC/255.0f*1000.0);
+            for(int i = 75; i <= 255; i++){
+                analogWrite(OUT_PIN, i);
+                delay(delayValue);
+                buttonStatus = (digitalRead(BUTTON_PIN) == HIGH);
+                if(!buttonStatus){
+                   break;
+                }
+            }
+
+            digitalWrite(OUT_PIN, HIGH);
+        }
+    }
+    if(!buttonStatus && enabled){
+        enabled = false;
+        digitalWrite(OUT_PIN, LOW);    
+    }
+}
